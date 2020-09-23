aa052f2c8363fb92f2b5975cde4bfcc95b32fd72
Oliver Rausch <oliverrausch99@gmail.com>
2020-09-24 00:14:32 +0200

    Port initial autodiff implementation

--- daceml/autodiff/implementation_abc.py
+++ daceml/autodiff/implementation_abc.py
@@ -9,22 +9,27 @@ from daceml.onnx import ONNXOp, SDFGState, SDFG
 
 @make_registry
 class ONNXBackward(abc.ABC):
-    """ ABC for ONNX op forward implementations.
+    """ABC for ONNX op forward implementations.
 
-        The register function expects an argument `op` containing the ONNX op name (string).
+    The register function expects an argument `op` containing the ONNX op name (string).
     """
+
     @staticmethod
     @abc.abstractmethod
-    def backward_can_be_applied(node: ONNXOp, state: SDFGState,
-                                sdfg: SDFG) -> bool:
+    def backward_can_be_applied(node: ONNXOp, state: SDFGState, sdfg: SDFG) -> bool:
 
         # TODO docstring
         ...
 
     @staticmethod
     @abc.abstractmethod
-    def backward(node: ONNXOp, state: SDFGState,
-                 sdfg: SDFG) -> typing.Union[Node, SDFG]:
+    def backward(
+        node: ONNXOp, state: SDFGState, sdfg: SDFG
+    ) -> typing.Union[Node, SDFG]:
 
         # TODO docstring
         ...
+
+
+# register the implementations
+import daceml.autodiff.implementations
